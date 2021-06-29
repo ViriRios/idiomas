f10c3621bd55c728f18c7549d17c1bf94bbd25bc
ViriRios <barbara.rios4808@alumnos.udg.mx>
2021-06-29 02:18:58 -0500

    Implementación de Vistas

--- resources/views/curso/cursoFrom.blade.php
+++ resources/views/curso/cursoFrom.blade.php
@@ -1,41 +1,87 @@
-@extends('layouts.temp')
+@extends('layouts.windmill')
 @section('contenido')
-    <h1>Formulario de Cursos</h1>
-
-    <p>
-        <a href="{{ route('curso.index') }}">Listado de Cursos</a>
-    </p>
-
+    <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+    <br>
+        Formulario
+    </h4>
+    <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800" >
     @if(isset($curso))
         <form action= "{{ route('curso.update', $curso) }}" method="POST"> 
              @method('PATCH')
     @else
         <form action= "{{ route('curso.store') }}" method="POST"> 
     @endif
+     @csrf
 
-        @csrf
-        <label for="grupo">Grupo:</label>
-        <input type="text" name="grupo" id="grupo" value= "{{$curso->grupo ?? '' }}">
+        <label class="block text-sm">
+            <span class="text-gray-700 dark:text-gray-400">Nombre del curso:</span>
+            <input
+            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+            type="text"
+            name="nombre"
+            id="nombre"
+            value="{{$curso->nombre ?? '' }}" 
+            />
+        </label>
         <br>
 
-        <label for="folio">Folio:</label>
-        <input type="text" name="folio" id="folio" value= "{{$curso->folio ?? '' }}">
+        <label class="block text-sm">
+            <span class="text-gray-700 dark:text-gray-400">Dependencia:</span>
+            <input
+            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+            type="text"
+            name="dependencia"
+            id="dependencia"
+            value="{{$curso->dependencia ?? '' }}" 
+            />
+        </label>
         <br>
 
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" id="nombre" value= "{{$curso->nombre ?? '' }}">
+        <label class="block text-sm">
+            <span class="text-gray-700 dark:text-gray-400">Grupo:</span>
+            <input
+            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+            type="text"
+            name="grupo"
+            id="grupo"
+            value="{{$curso->grupo ?? '' }}" 
+            />
+        </label>
         <br>
 
-        <label for="dependencia">Dependencia:</label>
-        <input type="text" name="dependencia" id="dependencia" value= "{{$curso->dependencia ?? '' }}">
+        <label class="block text-sm">
+            <span class="text-gray-700 dark:text-gray-400">Titular:</span>
+            <input
+            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+            type="text"
+            name="titular"
+            id="titular"
+            value="{{$curso->titular ?? '' }}" 
+            />
+        </label>
         <br>
 
-        <label for="titular">Titular:</label>
-        <input type="text" name="titular" id="titular" value= "{{$curso->titular ?? '' }}"> 
+        <label class="block text-sm">
+            <span class="text-gray-700 dark:text-gray-400">Folio del Curso:</span>
+            <input
+            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+            type="text"
+            name="folio"
+            id="folio"
+            value="{{$curso->folio ?? '' }}" 
+            />
+        </label>
+
         <br>
-
-        <input type="submit" value="Guardar">
-
+        <div class="mt-5">
+            <button class="flex items-center justify-between px-5 py-3 font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                type="submit"
+            >     
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path></svg>
+            <span>Guardar</span> 
+            </button>
+        </div>
     </form>
+</div>
 
 @endsection
--- resources/views/curso/cursoShow.blade.php
+++ resources/views/curso/cursoShow.blade.php
@@ -1,37 +1,39 @@
-@extends('layouts.temp')
+@extends('layouts.windmill')
 @section('contenido')
-    <h1>Detalles del Curso</h1>
 
-    <p>
-        <a href= "{{ route('curso.index') }}">Listado de Cursos</a>
-    </p>
-
-
-    <table border="1">
-        <thead>
-            <td>ID</td>
-            <td>Grupo</td>
-            <td>Folio</td>
-            <td>Nombre</td>
-            <td>Dependencia</td>
-            <td>Titular</td>
-        </thead>
-        <tbody> 
-            <tr>
-            <td>{{$curso->id}}</td>
-            <td>{{$curso->grupo}}</td>
-            <td>{{$curso->folio}}</td>
-            <td>{{$curso->nombre}}</td>
-            <td>{{$curso->dependencia}}</td>
-            <td>{{$curso->titular}}</td>
-        </tr>
-        </tbody>
-    </table>
+<h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+    Detalles del Curso
+</h2>
+    <div class="grid gap-6 mb-8 md:grid-cols-2">
+        <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+            <h4 class="mb-4 font-semibold text-gray-600 dark:text-gray-300">
+                {{ $curso->nombre }}
+            </h4>
+            <p class="text-gray-600 dark:text-gray-400">
+                <ul>
+                    <li>Dependencia: {{ $curso->dependencia }} </li>
+                    <li>Grupo: {{ $curso->grupo }} </li>
+                    <li>Titular: {{ $curso->titular }} </li>
+                    <li>Folio: {{ $curso->folio }} </li>
+                </ul>
+            </p>
+        </div>
 
     <form action= "{{ route('curso.destroy', $curso) }}" method="POST">
         @csrf
         @method('DELETE')
-        <input type="submit" value="Eliminar Curso">
+
+    <div>
+        <button    
+        class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+        type = "sumbit"    
+        >
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"></path>
+        </svg>              
+        <span>Eliminar Curso</span>
+    </button>
+    </div>
     </form>
 
 @endsection
