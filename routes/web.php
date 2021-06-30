6aac137b97019c4510bb1789400ed2f65ad0091a
ViriRios <barbara.rios4808@alumnos.udg.mx>
2021-06-30 15:41:58 -0500

    Verificación de Correo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\CursoController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +17,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 Route::get('/inicio', function() {
     return view('inicio');
 });
-
 
 Route::get('/', function () {
     return view('inicio');
@@ -29,4 +47,4 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 Route::post('curso/{curso}/agrega-estudiante', [CursoController::class, 'agregaEstudiante'])->name ('curso.agrega-estudiante'); 
 
-Route::resource('curso', CursoController::class)->middleware('auth');
+Route::resource('curso', CursoController::class)->middleware('verified');
